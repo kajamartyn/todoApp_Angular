9bd8c3e1bad598a4ee0e7d7f0d38be2b928c9a84
Kamila <infkago@gmail.com>
2021-02-26 23:21:08 +0100

    stworzenie listy zadań oraz komponentu  pojedyńczego zadania

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
+import { ListTaskComponent } from './list-task/list-task.component';
+import { ListTaskItemComponent } from './list-task/list-task-item/list-task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchComponent
+    SearchComponent,
+    ListTaskComponent,
+    ListTaskItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/list-task/list-task-item/list-task-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-task-item',
+  templateUrl: './list-task-item.component.html',
+  styleUrls: ['./list-task-item.component.scss']
+})
+export class ListTaskItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
